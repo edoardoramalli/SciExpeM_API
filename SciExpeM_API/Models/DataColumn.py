2d1600d06fd96c71ffcf2219ff5b4f0e0981b675
timoteo dinelli <timoteo.dinelli@polimi.it>
2022-12-28 14:41:58 +0100

    Major modifications
    Removed: diz (useless old stuff), add control over species object, modified serializer to ignore specific properties

--- SciExpeM_API/Models/DataColumn.py
+++ SciExpeM_API/Models/DataColumn.py
@@ -23,14 +23,16 @@ class DataColumn:
         self._uncertainty_kind = uncertainty_kind
         self._uncertainty_bound = uncertainty_bound
 
-        self._diz = None
         self._is_x = None
         self._is_y = None
 
         # -- Object
 
-        self._species_object = species_object if Tool.checkListType(species_object, Specie) \
-            else Tool.optimize(settings.DB, 'Specie', json.dumps(species_object), refresh=refresh)
+        if self._species == None:
+            pass
+        else:
+            self._species_object = species_object if Tool.checkListType(species_object, Specie) \
+                else Tool.optimize(settings.DB, 'Specie', json.dumps(species_object), refresh=refresh)
 
         if isinstance(uncertainty_reference, DataColumn):
             self._uncertainty_reference = uncertainty_reference
@@ -40,18 +42,6 @@ class DataColumn:
                 self._uncertainty_reference = tmp[0]
             else:
                 self._uncertainty_reference = None
-
-    @property
-    def diz(self):
-        if not self._diz:
-            self._diz = {
-                'header': self.name if self.name != 'composition' else '+'.join([specie.preferredKey for specie in self.species_object]),
-                'units': self.units,
-                'data': self.data
-            }
-            return self._diz
-        else:
-            return self._diz
 
     @property
     def id(self):
@@ -191,7 +181,7 @@ class DataColumn:
             return cls(**data_dict)
 
     def serialize(self):
-        return Tool.serialize(self, exclude=['id'])
+        return Tool.serialize(self, exclude=['id', 'is_x', 'is_y'])
 
     def __repr__(self):
         return f'<DataColumns ({self.id})>'
